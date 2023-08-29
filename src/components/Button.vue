0f13981f74a143eb13cca10ce8848b35bda66551
dirgaastya <dirga@digits.id>
2023-08-29 14:58:33 +0700

    feat : add, update, remove & filter todos

---
+++ src/components/Button.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps({
+    label:String
+})
+</script>
+<template>
+    <button class="py-1 px-4 rounded bg-gray-950 font-normal border border-transparent hover:border hover:border-white transition ease-in-out duration-300">
+        {{ label }}
+    </button>
+</template>
--- src/pages/Todos.vue
+++ src/pages/Todos.vue
@@ -1,5 +1,75 @@
 <script setup lang="ts">
+import { ref,onMounted, watch } from "vue";
+import axios from 'axios'
+import Button from '../components/Button.vue'
 
+type Todo = {
+    userId: number,
+    id: number,
+    title: string,
+    completed: boolean
+};
+
+const todos = ref<Todo[]>([]);
+const filteredTodos = ref<Todo[]>([])
+const title = ref<String>('')
+
+const getTodoList = async () :Promise<void> => {
+    const response = await axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+    todos.value = response.data
+    filteredTodos.value =response.data
+}
+
+const postTodo = async ():Promise<void> =>{
+    if(title.value !== ""){
+        const response = await axios.post("https://jsonplaceholder.typicode.com/todos",{
+        userId: 1,
+        title: title.value,
+        completed: false
+    })
+    title.value = ""
+    todos.value.unshift(response.data)
+    }else{
+        window.alert('Please fill title!');
+    }
+
+}
+
+const updateTodo = async (id:number):Promise<void> => {
+    await axios.put(`https://jsonplaceholder.typicode.com/todos/${id}`,{
+        completed: true
+    })
+}
+
+const deleteTodo = async (id:number):Promise<void> => {
+    await axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    filteredTodos.value = filteredTodos.value.filter(todo => todo.id !== id);
+}
+
+const filterAll = ():void => {
+    filteredTodos.value = todos.value
+}
+const filterActive = ():void => {
+    filteredTodos.value = todos.value.filter((todo)=>todo.completed!==true)
+}
+const filterCompleted = ():void => {
+    filteredTodos.value = todos.value.filter((todo)=>todo.completed===true)
+}
+const clearCompleted = ():void => {
+    todos.value = todos.value.filter((todo)=>todo.completed!==true)
+}
+
+function getTodosLength():number {
+    const todo = todos.value.filter(todo=>todo.completed !== true)
+    return todo.length
+}
+
+onMounted(()=>{
+    getTodoList()
+})
+watch(todos,()=>{
+    filteredTodos.value = todos.value
+})
 </script>
 
 <template>
@@ -8,25 +78,25 @@
             <h1 class="text-5xl font-thin text-center">TODOS</h1>
         </div>
         <div>
-            <input type="text" placeholder="What needs to be done" class="w-full h-12 px-3 rounded-md shadow-md">
+            <input type="text" @keyup.enter="postTodo" v-model="title" placeholder="What needs to be done" class="w-full h-12 px-3 rounded-md shadow-md">
         </div>
         <div class="py-3 flex items-center justify-between font-light">
-            <p>3 items left</p>
-            <ul class="flex items-center gap-x-3">
-                <li>All</li>
-                <li>Active</li>
-                <li>Completed</li>
-            </ul>
-            <button class="text-base">
+            <p>{{ getTodosLength() }} items left</p>
+            <div class="flex items-center gap-x-3">
+                <Button @click="filterAll" label="All" />
+                <Button @click="filterActive" label="Active" />
+                <Button @click="filterCompleted" label="Completed" />
+            </div>
+            <button class="text-base" @click="clearCompleted">
                 Clear Completed
             </button>
         </div>
-        <div class="w-full p-3 bg-zinc-700 flex items-center justify-between rounded-md shadow-md">
-            <div class="flex items-center gap-x-3">
-                <input type="checkbox">
-                <p class="text-base leading-relaxed tracking-tight">Something</p>
-            </div>
-            <p>delete</p>
-        </div>
+        <div v-for="todo in filteredTodos" :key="todo.id" class="w-full p-3 mb-3 bg-zinc-700 flex items-center justify-between rounded-md shadow-md">
+    <div class="flex items-center gap-x-3">
+        <input @change="updateTodo(todo.id)" type="checkbox" v-model="todo.completed">
+        <p :class="`text-base leading-relaxed tracking-tight ${ todo.completed ? 'line-through': ''}`">{{ todo.title }}</p>
+    </div>
+    <button @click="deleteTodo(todo.id)" class="text-base text-red-600 cursor-pointer">Delete</button>
+</div>
     </div>
 </template>
